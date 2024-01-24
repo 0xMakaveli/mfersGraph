4f2409863aa0d8a86b370dd4d84d94bdc2e0d5f3
0xMakaveli <makaveliboomboom@gmail.com>
2024-01-24 23:01:50 +0300

    Mappings.ts, yaml and graphql fixed

--- src/mappings.ts
+++ src/mappings.ts
@@ -1,4 +1,4 @@
-import {Transfer, Transfer__Params} from "../generated/Contract/Contract";
+import {Transfer, OwnershipTransferred} from "../generated/Contract/Contract";
 
 
 export function handleTransfer(event : Transfer): void {
@@ -7,5 +7,12 @@ export function handleTransfer(event : Transfer): void {
   entity.from = event.params.from;
   entity.to = event.params.to;
   entity.tokenId = event.params.tokenId
+  entity.save()
+}
+
+export function handleOwnershipTransfer(event : OwnershipTransferred): void {
+  let entity
+  entity.newOwner = event.params.newOwner
+  entity.previousOwner = event.params.previousOwner
   entity.save()
 }
