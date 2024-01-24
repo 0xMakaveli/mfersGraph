ae5d3f62273375a255216dac51fef17c4221e826
0xMakaveli <makaveliboomboom@gmail.com>
2024-01-24 08:08:54 +0300

    Added entity information

--- src/mappings.ts
+++ src/mappings.ts
@@ -2,5 +2,9 @@ import {Transfer, Transfer__Params} from "../generated/events.ts";
 
 
 export function handleTransfer(event : Transfer): void {
-  
+  let entity; 
+  entity.from = event.params.from;
+  entity.to = event.params.to;
+  entity.tokenId = event.params.tokenId
+  entity.save()
 }
