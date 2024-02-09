b9fc1df479def45bc0832e4e9c2908738e6d58a0
0xMakaveli <makaveliboomboom@gmail.com>
2024-02-09 23:23:26 +0300

    mfersTrait subgraph code deployements

--- src/mappings/mappings.ts
+++ src/mappings/mappings.ts
@@ -1,20 +1,82 @@
 import {Transfer as TransferEvent, Contract} from "../../generated/Contract/Contract";
-import {Token} from "../../generated/schema"
-import {TokenMetadata} from "../../generated/templates"
+import {Token,TokenMetadata} from "../../generated/schema"
+import {TokenMetadata as TokenMetadataTemplate} from "../../generated/templates"
+import { json, Bytes, dataSource } from '@graphprotocol/graph-ts'
+
 
 const ipfsHash = "QmbAvUyPA1E3Xe2bNhsHpnaqZYNpdwdfRjw3YGKaA9dMzk";
-
-export function handleTransfer(event : TransferEvent, cont: Contract): void {
-  const entity = new Token(event.transaction.hash.toHex());
+export function handleTransfer(event : TransferEvent): void {
+  const entity = new Token(event.transaction.hash.toString());
   entity.tokenID = event.params.tokenId;
-  entity.tokenURI = cont.tokenURI.toString();
+  entity.tokenURI = event.params.tokenId.toString(); 
   entity.updatedAtTimestamp = event.block.timestamp;
+  const ipfsHashURI  = ipfsHash + entity.tokenURI;
+  TokenMetadataTemplate.create(ipfsHashURI);
+  entity.save()
 
 }
 
-export function handleMetadata(call : TokenMetadata): void {
-  const entity = new TokenMetadata();
+export function handleMetadata(content: Bytes ): void {
+    let tokenmetadata  = new TokenMetadata(dataSource.stringParam());
+    const value = json.fromBytes(content).toObject();
+    if(value)
+      { 
+      const image = value.get("image");
+      const name = value.get("name");
+      const description = value.get("description");
+      const attributes  = value.get("attributes");
+      if(image && name && description && attributes) {
+            tokenmetadata.name = name.toString()
+            tokenmetadata.image = image.toString()
+            tokenmetadata.description = description.toString()
+            const attributesArray = attributes.toArray();
+            if(attributesArray) {
+              for (let i = 0; i < attributesArray.length; i++) {
+                const attributeObject = attributesArray[i].toObject();
+                const trait_type = attributeObject.get("trait_type");
+                const value = attributeObject.get("value");
+                if (trait_type && value) {
+                  switch (i) {
+                    case 0:
+                      tokenmetadata.trait_type_1 = trait_type.toString();
+                      tokenmetadata.value_1 = value.toString();
+                        break;
+                    case 1:
+                      tokenmetadata.trait_type_2 = trait_type.toString();
+                      tokenmetadata.value_2 = value.toString();
+                        break;
+                    case 2:
+                      tokenmetadata.trait_type_3 = trait_type.toString();
+                      tokenmetadata.value_3 = value.toString();
+                        break;
+                    case 3:
+                      tokenmetadata.trait_type_4 = trait_type.toString();
+                      tokenmetadata.value_4 = value.toString();
+                        break;
+                    case 4:
+                      tokenmetadata.trait_type_5 = trait_type.toString();
+                      tokenmetadata.value_5 = value.toString();
+                        break;
+                    case 5:
+                      tokenmetadata.trait_type_6 = trait_type.toString();
+                      tokenmetadata.value_6 = value.toString();
+                        break;
+                    case 6:
+                      tokenmetadata.trait_type_7 = trait_type.toString();
+                      tokenmetadata.value_7 = value.toString();
+                        break;
+                    case 7:
+                      tokenmetadata.trait_type_8 = trait_type.toString();
+                      tokenmetadata.value_8 = value.toString();
+                        break;
+                  }  
 
+                }
 
+            }
+        }
+        tokenmetadata.save();
+      }
 
+    }
 }
